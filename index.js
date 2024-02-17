90d5609df359041bb432506eb417babba41b71da
SymuNadeem <msymunnadeem@gmail.com>
2024-02-17 21:42:50 +0600

    reducer counter

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
 //defining constants
-const INCREMENT ="INCREMENT"
-const DECREMENT ="DECREMENT"
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const ADD_USER = "ADD_USER";
 
 //state:
-const initialCounterState ={
-      count: 0,
-}
+const initialCounterState = {
+  count: 0,
+};
 
-const initialUserState={
-  user:[{
-    name: "Iqbal"
-  }]
-}
+const initialUserState = {
+  user: [
+    {
+      name: "Iqbal",
+    },
+  ],
+};
 
 //action- object -type, payload
-const incrementCounter =()=>{
+const incrementCounter = () => {
   return {
-    type: INCREMENT
-  }
-}
-const decrementCounter =()=>{
+    type: INCREMENT,
+  };
+};
+const decrementCounter = () => {
   return {
-    type: DECREMENT
-  }
-}
+    type: DECREMENT,
+  };
+};
 
+const addUser = () => {
+  return {
+    type: ADD_USER,
+    payload: { name: "Pot" },
+  };
+};
+
+//reducer for counter
+
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+      case DECREMENT:
+        return {
+          ...state,
+          count: state.count - 1,
+        };
+
+    default:
+      state;
+  }
+};
