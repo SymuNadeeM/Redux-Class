6432b0cd270af5ce0eb979d92256bcf3e1a8eff6
SymuNadeem <msymunnadeem@gmail.com>
2024-02-18 19:44:34 +0600

    get data

--- fetchingData/index.js
+++ fetchingData/index.js
@@ -1,67 +1,90 @@
 // fetch data using redux-thunk::
 // ---------------------------------
 
+const { default: axios } = require("axios");
+const { createStore, applyMiddleware } = require("redux");
+const thunk = require("redux-thunk").default;
+
 //api url - https://jsonplaceholder.typicode.com/todos
 //middleware - redux-thunk
 //axios api
 
-const GET_TODO_REQUEST = "GET_TODO_REQUEST"
-const GET_TODO_SUCCESS = "GET_TODO_SUCCESS"
-const GET_TODO_FAILED = "GET_TODO_FAILED"
-
+const GET_TODO_REQUEST = "GET_TODO_REQUEST";
+const GET_TODO_SUCCESS = "GET_TODO_SUCCESS";
+const GET_TODO_FAILED = "GET_TODO_FAILED";
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 //1 STATES
 const initialTodosState = {
   toDos: [],
-  isLoading:false,
-  error:null
-}
-
+  isLoading: false,
+  error: null,
+};
 
 // ACTION
-const getTodoRequest =()=>{
+const getTodoRequest = () => {
   return {
-    type: GET_TODO_REQUEST
-  }
-}
+    type: GET_TODO_REQUEST,
+  };
+};
 
-const getTodoFailed =(error)=>{
+const getTodoFailed = (error) => {
   return {
     type: GET_TODO_FAILED,
-    payload: error
-  }
-}
-const getTodoSuccess =(toDos)=>{
+    payload: error,
+  };
+};
+const getTodoSuccess = (toDos) => {
   return {
     type: GET_TODO_SUCCESS,
-    payload: toDos
-  }
-}
+    payload: toDos,
+  };
+};
 
 //REDUCERS
-const todosReducer = (state= initialTodosState, action)=>{
+const todosReducer = (state = initialTodosState, action) => {
   switch (action.type) {
     case GET_TODO_REQUEST:
       return {
-       ...state,
-       isLoading:true
-      }
+        ...state,
+        isLoading: true,
+      };
     case GET_TODO_SUCCESS:
       return {
-       ...state,
-       isLoading:false,
-       toDos: action.payload
-      }
+        ...state,
+        isLoading: false,
+        toDos: action.payload,
+      };
     case GET_TODO_FAILED:
       return {
-       ...state,
-       isLoading:false,
-       error:action.payload
-      }
-    
-      
-  
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
     default:
-      return state
+      return state;
   }
-}
+};
+
+//async action creator
+const fetchData = () => {
+  return (dispatch) => {
+    dispatch(getTodoRequest());
+    axios.get(API_URL)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+};
+
+//STORE
+const store = createStore(todosReducer, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(fetchData());
